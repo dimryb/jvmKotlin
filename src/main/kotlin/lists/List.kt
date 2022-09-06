bf780b4edfb5d0ee407943860af7c1b355f17022
dmitry <dimryb@bk.ru>
2022-09-06 22:30:26 +0700

    Собственная реализация List

---
+++ src/main/kotlin/lists/List.kt
@@ -0,0 +1,32 @@
+package lists
+
+sealed class List<A> {
+    abstract fun isEmpty(): Boolean
+
+    private object Nil : List<Nothing>() {
+        override fun isEmpty() = true
+
+        override fun toString(): String = "[NIL]"
+    }
+
+    private class Cons<A>(
+        internal val head: A,
+        internal val tail: List<A>,
+    ) : List<A>() {
+        override fun isEmpty(): Boolean = false
+
+        override fun toString(): String = "[${toString("", this)}NIL]"
+
+        private tailrec fun toString(acc: String, list: List<A>): String =
+            when (list) {
+                is Nil -> acc
+                is Cons -> toString("$acc${list.head}, ", list.tail)
+            }
+        companion object {
+            fun <A> invoke(vararg az: A): List<A> =
+                az.foldRight(Nil as List<A>) {
+                    a: A, list: List<A> -> Cons(a, list)
+                }
+        }
+    }
+}
